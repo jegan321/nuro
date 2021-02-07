3df848bcb52ea322c100da7c8e00622624e5f177
John Egan <eganj@dnb.com>
2021-02-06 18:20:29 -0600

    Added test

--- __tests__/unit/create-element.test.js
+++ __tests__/unit/create-element.test.js
@@ -1,8 +1,6 @@
 const esmImport = require('esm')(module)
 const { createElementFactory } = esmImport('../../build/compiled/components/create-element.js')
 
-// TODO: add test for createElement with includes map
-
 test('should create a VNode object', () => {
   const createElement = createElementFactory(new Map())
   let vnode = createElement('div', {id: 'my-element', class: 'my-class'}, ['Foo'])
@@ -21,4 +19,21 @@ test('when give children as a scalar, should create a VNode object', () => {
   expect(vnode.attrs.id).toEqual('my-element')
   expect(vnode.attrs.class).toEqual('my-class')
   expect(vnode.children[0].text).toEqual('Foo')
+})
+
+test('when given Map of includes, should be able to use them in function', () => {
+  let includes = new Map()
+  class MyButton {
+    $template = '<button>My Button</button>'
+  }
+  includes.set('my-button', MyButton)
+  const createElement = createElementFactory(includes)
+  let vnode = createElement('div', {id: 'my-element', class: 'my-class'}, [
+    createElement('my-button')
+  ])
+  expect(vnode.nodeType).toEqual('element')
+  expect(vnode.tag).toEqual('div')
+  expect(vnode.attrs.id).toEqual('my-element')
+  expect(vnode.attrs.class).toEqual('my-class')
+  expect(vnode.children[0].componentClass).toEqual(MyButton)
 })
--- src/components/component-proxy.ts
+++ src/components/component-proxy.ts
@@ -2,8 +2,6 @@ import { ComponentClass } from '../api/component.js'
 import { CreateElement } from '../api/create-element.js'
 import { VNode } from '../api/vnode.js'
 
-// TODO: move this to components folder since it is not part of public API
-
 export interface ComponentProxy {
   props: Record<string, any>
   [state: string]: any
