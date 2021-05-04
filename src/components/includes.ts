93d329e45b4cad1a3d67f431c2e84c5751cb7551
John Egan <eganj@dnb.com>
2021-05-03 23:05:01 -0500

    Automatically convert PascalCase name to kebab-case when including a global component

--- src/components/includes.ts
+++ src/components/includes.ts
@@ -1,7 +1,9 @@
 import { ComponentClass } from '../api/component.js'
+import { Nuro } from '../index.js'
+import { camelCaseToKebabCase } from '../util/string-utils.js'
 
 export const globalIncludes = new Map<string, ComponentClass>()
 
 export function include(componentName: string, ComponentClass: ComponentClass): void {
-  globalIncludes.set(componentName, ComponentClass)
+  globalIncludes.set(camelCaseToKebabCase(componentName), ComponentClass)
 }
