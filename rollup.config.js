efa5cf23b628b2f68e6b890941d6d021fb15e319
John Egan <eganj@dnb.com>
2021-05-17 01:16:48 -0500

    Added minified versions to build

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,6 @@
+
+import { terser } from 'rollup-plugin-terser'
+
 export default [
   {
     input: 'compiled/index.js',
@@ -7,11 +10,28 @@ export default [
     }
   },
   {
+    input: 'compiled/index.js',
+    output: {
+      file: 'dist/nuro.min.js',
+      format: 'es'
+    },
+    plugins: [terser()]
+  },
+  {
     input: 'compiled/index-umd.js',
     output: {
       file: 'dist/nuro.umd.js',
       format: 'umd',
       name: 'Nuro'
     }
+  },
+  {
+    input: 'compiled/index-umd.js',
+    output: {
+      file: 'dist/nuro.umd.min.js',
+      format: 'umd',
+      name: 'Nuro'
+    },
+    plugins: [terser()]
   }
 ]
